5df4331b215111f26f3c3fb26c6020ea7231c062
vivek <vivek@verificient.com>
2018-12-04 22:48:16 +0530

    Proctortrack Custom JS - initial commit

---
+++ edx_proctoring_proctortrack/static/proctortrack_custom.js
@@ -0,0 +1,56 @@
+var initial_port = 54545;
+var pt_msg = 'It appears that the Proctortrack App is currently not running. Please go to the EDX exam page to start proctoring. If you have already taken the test, there is no further action required and you may continue with other coursework. If Proctortrack application is already running, refresh this page or contact Proctortrack support.'
+
+class ProctoringServiceHandler {
+  onStartExamAttempt() {
+    return checkAppStatus(initial_port);
+  }
+  onEndExamAttempt() {
+    return closePTApp(initial_port);
+  }
+  onPing() {
+    return checkAppStatus(initial_port);
+  }
+}
+
+var app_not_running_action = function () {
+    alert(pt_msg);
+};
+
+var app_running_action = function() {
+    Promise.resolve();
+};
+
+var closePTApp = function(initial_port){
+    var xmlhttp = new XMLHttpRequest();
+    var url = 'https://app.verificient.com:' + initial_port + '/proxy_server/app/close_proctoring/';
+    xmlhttp.open('GET', url, true);
+    xmlhttp.send();
+};
+
+var checkAppStatus = function (initial_port) {
+    var xmlhttp = new XMLHttpRequest();
+    var url = 'https://app.verificient.com:' + initial_port + '/proxy_server/app/proctoring_started/';
+    xmlhttp.open('GET', url, true);
+    xmlhttp.onreadystatechange = function () {
+
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
+            // app is running,
+            var data = JSON.parse(xmlhttp.responseText);
+            if (data.proctoring){
+                // proctoring started, notify edx
+                alert('PT APP is running but proctoring is yet not started');
+
+            } else{
+                // app is running but proctoring is not started, notify edx
+                app_not_running_action();
+            }
+        }
+        else if ((xmlhttp.readyState == 4 || xmlhttp.readyState =="complete")) {
+        //app is not running on any port and app server is down, notify edx
+            app_not_running_action();
+        }
+
+    };
+    xmlhttp.send();
+};
