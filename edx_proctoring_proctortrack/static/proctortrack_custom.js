c29ba23837f5c2ed43b8f4c2b56041a520f46224
Anup <anup@verificient.com>
2018-12-06 18:02:47 +0530

    indentation update

--- edx_proctoring_proctortrack/static/proctortrack_custom.js
+++ edx_proctoring_proctortrack/static/proctortrack_custom.js
@@ -15,12 +15,12 @@ var app_not_running_action = function () {
     return Promise.reject();
 };
 
-var app_running_action = function() {
+var app_running_action = function () {
     console.log('PT APP is running');
     return Promise.resolve();
 };
 
-var closePTApp = function(initial_port){
+var closePTApp = function (initial_port) {
     // this function is called to close the PT APP, On this Event, PT APP will upload the data and will close itself.
     var xmlhttp = new XMLHttpRequest();
     var url = 'https://app.verificient.com:' + initial_port + '/proxy_server/app/close_proctoring/';
@@ -33,44 +33,44 @@ var closePTApp = function(initial_port){
 var checkAppStatus = function (initial_port) {
     // this function is called to check the status of PT APP, if PT app is running it will return resolve else reject;
     // the function will also be called using setTimeout to check the status of PT app at defined interval.
-    if (!app_closed){
+    if (!app_closed) {
         var xmlhttp = new XMLHttpRequest();
         var url = 'https://app.verificient.com:' + initial_port + '/proxy_server/app/proctoring_started/';
         xmlhttp.open('GET', url, true);
         xmlhttp.onreadystatechange = function () {
 
-            if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
                 // app is running, notify edx
                 var data = JSON.parse(xmlhttp.responseText);
                 is_initial_check = false;
-                if (data.proctoring){
+                if (data.proctoring) {
                     // proctoring started, notify edx
                     app_running_action();
 
                     attempt_count = 0;  // reset attempt count
                     // set 'checkAppStatus' function to be called at 'standard_timeout' i.e 30 seconds,
                     // this is done to check if PT app is running while student is attempting the exam.
-                    setTimeout(function(){
+                    setTimeout(function () {
                         checkAppStatus(initial_port);
                     }, standard_timeout);
-                } else{
+                } else {
                     // app is running but proctoring is not started, notify edx
                     app_not_running_action();
                 }
             }
-            else if ((xmlhttp.readyState == 4 || xmlhttp.readyState =="complete")) {
+            else if ((xmlhttp.readyState == 4 || xmlhttp.readyState == "complete")) {
                 //app is not running on any port and app server is down, notify edx
-                if (is_initial_check){
+                if (is_initial_check) {
                     app_not_running_action();
                     is_initial_check = false;
-                } else if ( attempt_count < max_attempt_count) {    // check five more times before returning Promise.reject();.
+                } else if (attempt_count < max_attempt_count) {    // check five more times before returning Promise.reject();.
                     // set 'checkAppStatus' function to be called at 'failure_timeout' i.e 30 seconds,
                     // this is done to check if PT app is re-started by 'watchdog process'
-                    setTimeout(function(){
+                    setTimeout(function () {
                         checkAppStatus(initial_port);
                     }, failure_timeout);
-                    attempt_count +=1;
-                } else{
+                    attempt_count += 1;
+                } else {
                     app_not_running_action();
                 }
             }
@@ -80,15 +80,17 @@ var checkAppStatus = function (initial_port) {
 };
 
 class PTProctoringServiceHandler {
-  onStartExamAttempt() {
-    checkAppStatus(initial_port);
-  }
-  onEndExamAttempt() {
-    closePTApp(initial_port);
-  }
-  onPing() {
-    checkAppStatus(initial_port);
-  }
+    onStartExamAttempt() {
+        checkAppStatus(initial_port);
+    }
+
+    onEndExamAttempt() {
+        closePTApp(initial_port);
+    }
+
+    onPing() {
+        checkAppStatus(initial_port);
+    }
 }
 
 export default handlerWrapper(PTProctoringServiceHandler);
