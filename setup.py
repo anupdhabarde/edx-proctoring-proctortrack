4df8e7183e84e8a67f1537174bb0cb8f9a42fc92
Vivek Joshi <vivek@verificient.com>
2019-03-13 16:29:37 +0530

    Update setup.py
    Updated Version in setup file.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     url='https://github.com/joshivj/edx-proctoring-proctortrack',
     license="Apache-2.0",
     keywords='Proctortrack edx',
-    version='1.0.2',
+    version='1.0.3',
     packages=[
         'edx_proctoring_proctortrack',
     ],
